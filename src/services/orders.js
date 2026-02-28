38c029dbf6aab508d1d8681dee02b7770fec531c
MOHD <s220006642@seu.edu.sa.com>
2026-02-28 21:20:42 +0300

    update

--- src/services/orders.js
+++ src/services/orders.js
@@ -10,12 +10,13 @@ import { db } from "../firebase/firebase";
 export async function createOrder(truckId, data) {
   const truckRef = doc(db, "foodTrucks", truckId);
   const truckSnap = await getDoc(truckRef);
-  const vendorId = truckSnap.data()?.vendorId;
+
+  const vendorId = truckSnap.data().vendorId; // لازم يكون موجود
 
   await addDoc(collection(db, "foodTrucks", truckId, "orders"), {
     ...data,
     truckId,
-    vendorId,                 // لازم يكون string
+    vendorId, // 🔥 هذا أهم سطر
     status: "pending",
     createdAt: serverTimestamp(),
   });
